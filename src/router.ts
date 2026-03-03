c645f2f32e9ffd00033816b4b5a0f998bde1dcc5
Benjamin Séché <seche.benjamin@gmail.com>
2026-03-03 11:11:43 +0100

    add route names

--- src/router.ts
+++ src/router.ts
@@ -3,9 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', redirect: '/products' },
-    { path: '/products', component: () => import('./views/ProductsView.vue') },
-    { path: '/products/:id', component: () => import('./views/ProductDetailView.vue') },
-    { path: '/favorites', component: () => import('./views/FavoritesView.vue') },
+    { name: "home", path: '/', redirect: '/products' },
+    { name: "products", path: '/products', component: () => import('./views/ProductsView.vue') },
+    { name: "productDetail", path: '/products/:id', component: () => import('./views/ProductDetailView.vue') },
+    { name: "favorits", path: '/favorites', component: () => import('./views/FavoritesView.vue') },
   ],
 })
