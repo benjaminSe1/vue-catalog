06f87e249ec061d49ba66588cbbd1959ce354ee3
Benjamin Séché <seche.benjamin@gmail.com>
2026-03-04 14:13:24 +0100

    fix after product store refacto

--- src/components/ProductSort.vue
+++ src/components/ProductSort.vue
@@ -1,22 +1,16 @@
 <script setup lang="ts">
-import type { ProductsState } from "../stores/products.store"
+import type { SortMode } from "../stores/products.store"
 
-const model = defineModel<ProductsState["sort"]>()
+const model = defineModel<SortMode>()
 
 const sortKeys = [
-  { value: "priceAsc", title: "Price: Low to High" },
-  { value: "priceDesc", title: "Price: High to Low" },
-  { value: "ratingDesc", title: "Rating: High to Low" },
+    { value: "priceAsc", title: "Price: Low to High" },
+    { value: "priceDesc", title: "Price: High to Low" },
+    { value: "ratingDesc", title: "Rating: High to Low" },
 ] as const
 </script>
 
 <template>
-  <h2>Sort By</h2>
-  <v-select
-    v-model="model"
-    :items="sortKeys"
-    item-title="title"
-    item-value="value"
-    label="Select Sort"
-  />
+    <h2>Sort By</h2>
+    <v-select v-model="model" :items="sortKeys" item-title="title" item-value="value" label="Select Sort" />
 </template>
